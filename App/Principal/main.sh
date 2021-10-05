32c2f8928ee4d410d8c9af80f43bc17b7a8a86c9
Barros7 <josebarros412@gmail.com>
2021-10-05 20:18:37 +0100

    Menu principal criado

---
+++ App/Principal/main.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#============ Função para  Sair do programa ============
+Sair(){
+    clear
+    exit
+}
+#============ Função do Menu_Iniciar ============
+Menu_Iniciar(){
+    echo " 1 - Compra                       "
+    echo " 2 - Venda                        "
+    echo " 3 - Atualizar Preço de Restauro  "
+    echo " 4 - Alterar Dados                "
+    echo " 5 - Visualizar Automoveis        "
+    echo " 6 - Gestão de Base de Dados      "
+    echo " 7 - Relatórios                   "
+    echo " 0 - Sair do Programa             "
+}
+#============ Função para criar Backup dos dados ============
+
+#============ Função para fazer o Restauro dos dados ============
+
+#============ Função para executar o programa ==============
